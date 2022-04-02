c9630fa9fae8715d774ccd2919898d4b7fdb8bb9
Haik Ivanyan <samplelogo19@gmail.com>
2022-04-02 15:26:16 +0300

    done 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
 
+player.on('timeupdate', throttle(currentTime, 1000));
+
+function currentTime(e) {
+  console.log(e.seconds);
+  localStorage.setItem('videoplayer-current-time', e.seconds);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
